f02065d5c92588ca31d238547c06f793e2326b6f
GavilanesJr <jggavilanes10@hotmail.com>
2023-11-25 20:18:01 -0600

    menu consultas

--- components/menu.js
+++ components/menu.js
@@ -1,47 +1,79 @@
 import Styles from "./menu.module.css"
 
-export default function CMenu(){
+export default function CMenu({data}){
+    const categorias = data.categorias.data;
+
     return (
         <main className={Styles.container}>
             <h2
                 className="Titulo"
-                style={{ color : `color1`}}
+                style={{ color : data.color1}}
             >Menú</h2>
                 <section
                     className={Styles.categorias}
                 >
                     <hr
                         className={Styles.separador}
-                        style={{ backgroundColor : `color3`}}
+                        style={{ backgroundColor : data.fondo}}
                     />
-                    <section
-                        className={Styles.gridTemplate}>
-                        <h3
-                            className={Styles.categoria_titulo}
-                            style={{ color: `color2` }}
-                        >Productos</h3>
-                        <h3 className={Styles.categoria_precio}>Precio</h3>
-                    </section>
-                        <section
-                            className={Styles.gridTemplate}>
-                            <section className={Styles.categoria_producto}>
-                                <h4 className={Styles.categoria_producto_titulo}>Nombre</h4>
-                                <p className={Styles.categoria_producto_desc}>Descripcion</p>
-                            </section>
-                            <h4 className={Styles.categoria_producto_precio}>$1.03</h4>
-                        </section>
-                    <section
-                        className={Styles.gridTemplate}>
-                        <h3
-                            className={Styles.categoria_producto_adicionales}
-                        >Adicionales</h3>
-                    </section>
-                        <section
-                            className={Styles.gridTemplate}
-                        >
-                            <h4 className={Styles.categoria_producto_titulo}>Nombre</h4>
-                            <h4 className={Styles.categoria_producto_precio}>$2.50</h4>
-                        </section>
+                    {
+                        categorias.map((categoria) => {
+                            const productos = categoria.attributes.productos.data;
+                            const adicionales = categoria.attributes.extras.data;
+
+                            return (
+                                <>
+                                    <section
+                                        className={Styles.gridTemplate}>
+                                        <h3
+                                            className={Styles.categoria_titulo}
+                                            style={{ color: data.color2 }}
+                                        >{categoria.attributes.nombre}</h3>
+                                        <h3 className={Styles.categoria_precio}>Precio</h3>
+                                    </section>
+                                    {
+                                        productos
+                                        .map((producto) => {
+                                            return (
+                                                <section
+                                                    className={Styles.gridTemplate}>
+                                                    <section className={Styles.categoria_producto}>
+                                                        <h4 className={Styles.categoria_producto_titulo}>{producto.attributes.nombre}</h4>
+                                                        <p className={Styles.categoria_producto_desc}>{producto.attributes.descripcion}</p>
+                                                    </section>
+                                                    <h4 className={Styles.categoria_producto_precio}>${producto.attributes.precio}</h4>
+                                                </section>
+                                            )
+                                        })
+                                    }
+                                    {
+                                        adicionales.map((adicional) => {
+                                            return (
+                                                <>
+                                                    <section
+                                                        className={Styles.gridTemplate}>
+                                                        <h3
+                                                            className={Styles.categoria_producto_adicionales}
+                                                        >Adicionales</h3>
+                                                    </section>
+                                                    <section
+                                                        className={Styles.gridTemplate}
+                                                    >
+                                                        <h4 className={Styles.categoria_producto_titulo}>{adicional.attributes.nombre}</h4>
+                                                        <h4 className={Styles.categoria_producto_precio}>${adicional.attributes.precio}</h4>
+                                                    </section>
+                                                </>
+                                            )
+                                        })
+                                    }
+                                    <hr
+                                        className={Styles.separador}
+                                        style={{ backgroundColor : data.fondo}}
+                                    />
+                                </>
+                            )
+                        })
+                    }
                 </section>
         </main>
     )
--- pages/restaurante/[slug].js
+++ pages/restaurante/[slug].js
@@ -1,16 +1,78 @@
 import NavPublic from "/components/navPublic"
 import GuppyDigital from "/components/guppydigital"
 import CMenu from "@/components/menu";
+import icons from "@/components/icons";
         
 
-const Restaurante = ({ restaurante }) => {
+const Restaurante = () => {
+    const restaurante = {
+        'nombre' : 'Res1',
+        'url_img' : null,
+        "color1": "#008ADD",
+        "color2": "#2598DD",
+        "fondo": "#E4EFFF",
+        "categorias": {
+            "data": [
+                {
+                    "id": 1,
+                    "attributes": {
+                        "nombre": "Hamburguesas",
+                        "productos": {
+                            "data": [
+                                {
+                                    "id": 1,
+                                    "attributes": {
+                                        "nombre": "hamburguesa sencilla",
+                                        "descripcion": null,
+                                        "precio": 1.50,
+                                    }
+                                },
+                                {
+                                    "id": 2,
+                                    "attributes": {
+                                        "nombre": "hamburguesa completa",
+                                        "descripcion": 'Pan, salsas, carne de res, lechuga, tomate, mozarella, pollo, cheedar, carne de cerdo, tocino, chorizo, chimichurri.',
+                                        "precio": 7,
+                                    }
+                                }
+                            ]
+                        },
+                        "extras": {
+                            "data": [
+                                {
+                                    "id": 1,
+                                    "attributes": {
+                                        "nombre": "salsa picante",
+                                        "precio": 10,
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "id": 2,
+                    "attributes": {
+                        "nombre": "Hotdogs",
+                        "productos": {
+                            "data": []
+                        },
+                        "extras": {
+                            "data": []
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
     return(
-        <div style={{background:`color3`}}>
+        <div style={{background:`${restaurante.fondo}`}}>
             <NavPublic
-                logo="logo"
-                title="title"
+                logo={(restaurante.url_img != null) ? restaurante.url_img : icons.locals}
+                title={restaurante.nombre}
             />
-            <CMenu />
+            <CMenu data={restaurante}/>
             <GuppyDigital />
         </div>
     )
